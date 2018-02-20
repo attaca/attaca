de5524512284867d0fa07b6ad1d61f172e8048e4
Sean Leffler <sean@errno.com>
2018-02-20 08:12:30 -0800

    [attaca-test] initial

---
+++ attaca-test/src/lib.rs
@@ -0,0 +1,6 @@
+extern crate attaca;
+#[macro_use]
+extern crate failure;
+extern crate proptest;
+
+pub mod store;
---
+++ attaca-test/src/store.rs
@@ -0,0 +1,4 @@
+use attaca::Store;
+use failure::Error;
+
+// pub fn send_then_fetch_small<S: Store>(store: &S, data: Vec<u8>) -> 
